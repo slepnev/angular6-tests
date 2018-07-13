502e2514853591205c143ca7d9a9d831e272037e
Alexander <slepnev-alexander@mail.ru>
2018-07-13 08:20:49 +0300

    E2E

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,67 @@
-import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
-describe('workspace-project App', () => {
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+
+  getSendButton() {
+    return element(by.css('app-root .button-send'));
+  }
+
+  getAddButton() {
+    return element(by.css('app-root .button-add'));
+  }
+
+  getResault() {
+    return element(by.css('.result'));
+  }
+
+  getResaultElements() {
+    return element.all(by.css('.result > .result__item'));
+  }
+}
+
+describe('App Page e2e: "/"', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-tests!');
+  it('App title', () => {
+    expect(browser.getTitle()).toEqual('AngularTests');
+  });
+
+  it('App H1 check', () => {
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+
+  it('App check send button', () => {
+    expect(page.getSendButton().isDisplayed()).toBeTruthy();
+  });
+
+  it('App check add button', () => {
+    expect(page.getAddButton().isDisplayed()).toBeTruthy();
+  });
+
+  it('App click send button', () => {
+    page.getSendButton().click();
+    expect(page.getResault().isDisplayed()).toBeTruthy();
+    expect(page.getResaultElements().count()).toBeGreaterThan(10);
+  });
+
+  it('App click add button', () => {
+    page.getSendButton().click();
+    const count = page.getResaultElements().count();
+    console.log(count);
+    page.getAddButton().click();
+    expect(page.getResault().isDisplayed()).toBeTruthy();
+    expect(page.getResaultElements().count()).toBeGreaterThan(count);
   });
 });
